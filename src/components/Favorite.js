84a78794ba7a172fa21a7ddd49c75f9a2517b11f
dansilvaUT <dan13silv@gmail.com>
2021-01-01 10:22:20 -0800

    added styling to favorites and favorite compontents

--- src/components/Favorite.js
+++ src/components/Favorite.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import './Favorite.css';
 
 class Favorite extends Component {
     constructor() {
@@ -33,18 +34,18 @@ class Favorite extends Component {
     render() {
         const { person } = this.props;
         return (
-            <section>
+            <section class="favorite-container">
                 <h3>{person.name}</h3>
-                <img src={person.image} alt={person.name} className="character-image" />
+                <img src={person.image} alt={person.name} className="favorite-image" />
                 <h4>Status: {person.status}</h4>
                 {this.state.isEditing
                     ? (<section>
                         <input type="text" name={this.state.input} onChange={e => this.handleNameChange(e.target.value)} />
-                        <button onClick={() => this.editName(person.id)}>Change Name</button>
+                        <button onClick={() => this.editName(person.id)} className="change-btn">Change Name</button>
                     </section>)
                     : (<section>
-                        <button onClick={this.toggleEdit}>Edit Name</button>
-                        <button onClick={() => this.deleteCharacter(person.id)}>Delete from Favorites</button>
+                        <button onClick={this.toggleEdit} className="edit-btn">Edit Name</button>
+                        <button onClick={() => this.deleteCharacter(person.id)} className="delete-btn">Delete from Favorites</button>
                     </section>)}
             </section>
         )
--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,18 +1,23 @@
 import Favorite from './Favorite';
+import './Favorites.css';
 
 const Favorites = props => {
     return (
         <section>
-            {props.favorites.map((person, index) => {
-                return (
-                    <Favorite
-                        key={index}
-                        person={person}
-                        edit={props.edit}
-                        delete={props.delete}
-                    />
-                )
-            })}
+            <h1>My Favorites</h1>
+            <div className="favorites-container">
+                {props.favorites.map((person, index) => {
+                    return (
+                        <Favorite
+                            key={index}
+                            person={person}
+                            edit={props.edit}
+                            delete={props.delete}
+                        />
+                    )
+                })}
+            </div>
+
         </section>
     )
 }
