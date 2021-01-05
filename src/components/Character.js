f43e775567ac47173133f77d19b0b73a300ad8f4
dansilvaUT <dan13silv@gmail.com>
2021-01-05 08:53:18 -0800

    More styling and displayed appearances

--- src/components/Character.js
+++ src/components/Character.js
@@ -23,6 +23,12 @@ class Character extends Component {
                     <h5>Status: <span className={this.props.person.status === 'Deceased' ? "deceased" : "alive"}>
                         {this.props.person.status}</span>
                     </h5>
+                    <h4>Appearances:</h4>
+                    <ul className="season-list">
+                        {this.props.person.appearance.map((season, index)=>(
+                            <li key={index}>Season - {season}</li>
+                        ))}
+                    </ul>
                 </div>
                 <button className="add-character" onClick={this.addCharacter}>Add to Favorites</button>
                 <button className="next-btn" onClick={() => this.props.get()}>Next Character</button>
